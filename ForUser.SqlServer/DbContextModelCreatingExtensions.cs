07f9d702575a47dc3edd273afe630d42d02ae219
文博 王 <1751723095@qq.com>
2025-10-17 17:24:47 +0800

    添加bulder.entity

--- ForUser.SqlServer/DbContextModelCreatingExtensions.cs
+++ ForUser.SqlServer/DbContextModelCreatingExtensions.cs
@@ -16,7 +16,15 @@ namespace ForUser.SqlServer
                 throw new ArgumentNullException(nameof(builder));
             builder.Entity<UserEntity>(entity =>
             {
-            
+                entity.ToTable("T_User");
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Code).HasMaxLength(20);
+                entity.Property(e => e.Name).HasMaxLength(50);
+                entity.Property(e => e.Mobile).HasMaxLength(20);
+                entity.Property(e => e.Password).HasMaxLength(50);
+                entity.Property(e => e.PasswordHash).HasMaxLength(50);
+                entity.Property(e => e.CreateName).HasMaxLength(20);
+                entity.Property(e => e.ModifierName).HasMaxLength(20);
             });
         }
     }
