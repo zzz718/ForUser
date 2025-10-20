a94ef0241335aac667adde05b2f5cb783461b761
文博 王 <1751723095@qq.com>
2025-10-20 08:51:23 +0800

    添加通用返回信息，分页，用户Dto

---
+++ ForUser.Application/Users/UserService.cs
@@ -0,0 +1,20 @@
+﻿using ForUser.Domains.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForUser.Application.Users
+{
+    public class UserService:IUserService
+    {
+        private readonly IUserRepository _userRepository;
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public Task CreateUser(User user)
+    }
+}
